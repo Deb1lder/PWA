8f3a9206fe099e13f8c236e8a5e6be4dbe73fb73
Deb1lder <dtomislav434@gmail.com>
2026-03-25 10:14:58 +0100

    1. vjezba za pwa

---
+++ vjezba1.php
@@ -0,0 +1,10 @@
+<?php
+$naslov = "Moj prvi PHP dokument";
+$autor = "Tomislav Delač";
+echo "<h1>$naslov</h1>";
+echo "<p>Ovu stranicu izradio je $autor.</p>";
+echo '<a href="https://theuselessweb.com/"
+target="_blank">Posjeti beskorisnu stranicu</a>';
+?>
+
+<!-- Naziv datoteke: vjezba1.php -->
